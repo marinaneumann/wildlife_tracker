3e314231689490455edf4ce5d96f94ad7bddeb96
Jeremy Whitaker & Marina Neumann <student@epicodus.com>
2014-08-29 12:17:49 -0700

    validate class attributes

---
+++ app/models/sighting.rb
@@ -0,0 +1,4 @@
+class Sighting < ActiveRecord::Base
+  validates :animal_id, :presence => true
+  validates :region_id, :presence => true
+end
---
+++ spec/sighting_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Sighting do
+  it { should validate_presence_of :animal_id }
+  it { should validate_presence_of :region_id }
+end
